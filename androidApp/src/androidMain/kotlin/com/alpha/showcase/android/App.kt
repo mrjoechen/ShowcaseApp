e3e79f7c3ef5440887c3a3e4c3d59d99ac8b4f99
mrjoechen <mrjoechen@qq.com>
2024-06-13 15:19:47 +0800

    [update] remove unused code

--- androidApp/src/androidMain/kotlin/com/alpha/showcase/android/App.kt
+++ androidApp/src/androidMain/kotlin/com/alpha/showcase/android/App.kt
@@ -4,13 +4,8 @@ import AndroidApp
 import android.app.Application
 
 class App: Application() {
-  companion object {
-    lateinit var INSTANCE: App
-  }
-
   override fun onCreate() {
     super.onCreate()
-    INSTANCE = this
     AndroidApp = this
   }
 }
