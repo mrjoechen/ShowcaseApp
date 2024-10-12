c89284d3e13fc778ed807040cd7841316172fc81
mrjoechen <mrjoechen@qq.com>
2024-10-12 19:47:38 +0800

    [update] ios log startup

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,7 +1,12 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import com.alpha.showcase.common.Startup
 
-fun MainViewController() = ComposeUIViewController { MainApp() }
+fun MainViewController() = run {
+    onStartup()
+    ComposeUIViewController {
+        MainApp()
+    }
+}
 
 fun onStartup() {
     Startup.run()
