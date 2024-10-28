4bed2e8e97c50444bfa23f913f0c18c01bc888aa
mrjoechen <mrjoechen@qq.com>
2024-10-28 21:47:58 +0800

    [fix]

--- composeApp/src/commonMain/kotlin/com/alpha/showcase/common/repo/NativeWebdavSourceRepo.kt
+++ composeApp/src/commonMain/kotlin/com/alpha/showcase/common/repo/NativeWebdavSourceRepo.kt
@@ -1,7 +1,11 @@
 package com.alpha.showcase.common.repo
 
 import com.alpha.showcase.common.networkfile.WebDavClient
+import com.alpha.showcase.common.networkfile.WebDavFile
 import com.alpha.showcase.common.networkfile.storage.remote.WebDav
+import io.ktor.http.Url
+import io.ktor.http.fullPath
+import io.ktor.http.hostWithPort
 
 class NativeWebdavSourceRepo : SourceRepository<WebDav, String> {
 
@@ -23,9 +27,11 @@ class NativeWebdavSourceRepo : SourceRepository<WebDav, String> {
             if (contents.isNotEmpty()) {
                 if (recursive) {
                     val recursiveContent = mutableListOf<String>()
+                    val urlWithoutPath = remoteApi.url.replace(Url(remoteApi.url).fullPath, "")
+                    val subClient = WebDavClient(urlWithoutPath, remoteApi.user, remoteApi.passwd)
                     contents.forEach {
                         if (it.isDirectory) {
-                            val subFiles = webDavClient.listFiles("${remoteApi.path}/${it.path}")
+                            val subFiles = traverseDirectory(subClient, it.path)
                             recursiveContent.addAll(subFiles.map { subFile -> subFile.path })
                         } else {
                             recursiveContent.add(it.path)
@@ -35,16 +41,36 @@ class NativeWebdavSourceRepo : SourceRepository<WebDav, String> {
                         filter?.let { f ->
                             filter { f(it) }
                         } ?: this
-                    }.map { "${remoteApi.url}/$it" })
+                    }.map { "${remoteApi.url.replace(Url(remoteApi.url).fullPath, "")}/$it" })
                 } else {
-                    val stringList = contents.filter { filter?.invoke(it.path) ?: true }.map { "${remoteApi.url}/${it.path}" }
-                    Result.success(stringList)
+                    val stringList = contents.filter { filter?.invoke(it.path) ?: true }
+                        .map { "${remoteApi.url.replace(Url(remoteApi.url).fullPath, "")}/${it.path}" }
+                    if (stringList.isEmpty()) {
+                        Result.failure(Exception("No content found."))
+                    } else Result.success(stringList)
                 }
             } else {
                 Result.failure(Exception("No content found."))
             }
         } catch (e: Exception) {
-            Result.failure(e)
+            e.printStackTrace()
+            Result.failure(Exception(e.message))
         }
+    }
+
+    private suspend fun traverseDirectory(webDavClient: WebDavClient, path: String): List<WebDavFile> {
+        val result = mutableListOf<WebDavFile>()
+        val resources: List<WebDavFile> = webDavClient.listFiles(path)
+        resources.forEach {
+            if (it.path != path) {
+                if (it.isDirectory) {
+                    result.addAll(traverseDirectory(webDavClient, it.path))
+                } else {
+                    result.add(it)
+                }
+            }
+
+        }
+        return result
     }
 }
