66e90cc996b2bb3fd68161dd8bf0ac469b9772ce
mrjoechen <mrjoechen@qq.com>
2025-09-16 20:08:11 +0800

    fix: weibo api error

--- showcase-api/src/commonMain/kotlin/com/alpha/showcase/api/weibo/WeiboApi.kt
+++ showcase-api/src/commonMain/kotlin/com/alpha/showcase/api/weibo/WeiboApi.kt
@@ -4,19 +4,24 @@ import com.alpha.showcase.api.BaseHttpClient
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.http.HttpHeaders
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 private const val WEIBO_BASE_URL = "https://m.weibo.cn/"
 
 class WeiboApi() : BaseHttpClient() {
 
+    @OptIn(ExperimentalUuidApi::class)
     suspend fun getUserInfo(uid: String): WeiboUserResponse {
         return get(WEIBO_BASE_URL + "api/container/getIndex") {
             header(HttpHeaders.ContentType, "application/json")
+            header("Cookie", "SUB=${Uuid.random()}")
             parameter("type", "uid")
             parameter("value", uid)
         }
     }
 
+    @OptIn(ExperimentalUuidApi::class)
     suspend fun getUserPosts(
         uid: String,
         containerid: String,
@@ -28,6 +33,7 @@ class WeiboApi() : BaseHttpClient() {
             parameter("type", "uid")
             parameter("value", uid)
             parameter("containerid", containerid)
+            header("Cookie", "SUB=${Uuid.random()}")
             page?.let { parameter("page", it) }
             sinceId?.let { parameter("since_id", it) }
         }
